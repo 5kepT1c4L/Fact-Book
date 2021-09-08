da71f56bebd39cf977b94faa5154bbc40020ae7e
Eric Jing <70067413+5kepT1c4L@users.noreply.github.com>
2021-09-08 11:53:12 -0400

    Back end development finished

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 from tkinter import *
 
+fact_turn = 20
+
 fact_list = []
 
 info_website = requests.get('https://bestlifeonline.com/useless-facts/').text
@@ -17,7 +19,7 @@ for fact in facts:
     fact_list.append(the_fact)
 
 
-print(fact_list)
+
 
 
 
@@ -25,6 +27,98 @@ tk = Tk()
 tk.title('Fact Book')
 tk.geometry('500x498')
 tk.configure(bg="white")
+
+
+#Button functions
+def forward():
+
+    global fact_turn
+    global fact_label
+
+    fact_label.place_forget()
+
+    fact_turn += 1
+
+    if len(fact_list[fact_turn].split()) > 7 and len(fact_list[fact_turn].split()) < 13:
+
+        fact_split = fact_list[fact_turn].split()
+
+        fact_split.insert(6, "\n")
+
+        final = " ".join(str(word) for word in fact_split)
+
+        fact_label = Label(fact_frame, text=final, font=('Times', 15))
+
+        fact_label.place(x=100, y=150)
+
+    if len(fact_list[fact_turn].split()) <= 7:
+
+        fact_label = Label(fact_frame, text=fact_list[fact_turn], font=('Times', 15))
+
+        fact_label.place(x=100, y=150)
+
+    if len(fact_list[fact_turn].split()) >= 13:
+
+        fact_split = fact_list[fact_turn].split()
+
+        fact_split.insert(6, "\n")
+
+        fact_split.insert(12, "\n")
+
+        final = " ".join(str(word) for word in fact_split)
+
+        fact_label = Label(fact_frame, text=final, font=('Times', 15))
+
+        fact_label.place(x=100, y=150)
+
+def back():
+
+    global fact_turn
+    global fact_label
+
+    fact_label.place_forget()
+
+    fact_turn -= 1
+
+    if len(fact_list[fact_turn].split()) > 7 and len(fact_list[fact_turn].split()) < 13:
+
+        fact_split = fact_list[fact_turn].split()
+
+        fact_split.insert(6, "\n")
+
+        final = " ".join(str(word) for word in fact_split)
+
+        fact_label = Label(fact_frame, text=final, font=('Times', 15))
+
+        fact_label.place(x=100, y=150)
+
+    if len(fact_list[fact_turn].split()) <= 7:
+
+        fact_label = Label(fact_frame, text=fact_list[fact_turn], font=('Times', 15))
+
+        fact_label.place(x=100, y=150)
+
+    if len(fact_list[fact_turn].split()) >= 13:
+
+        fact_split = fact_list[fact_turn].split()
+
+        fact_split.insert(6, "\n")
+
+        fact_split.insert(12, "\n")
+
+        final = " ".join(str(word) for word in fact_split)
+
+        fact_label = Label(fact_frame, text=final, font=('Times', 15))
+
+        fact_label.place(x=100, y=150)
+
+
+
+
+
+
+
+
 
 
 #Frame to store the fact
@@ -40,18 +134,18 @@ button_frame.pack_propagate(False)
 
 
 #Buttons
-back_button = Button(button_frame, text="<<", width=20, height=7)
+back_button = Button(button_frame, text="<<", width=20, height=7, command=back)
 back_button.grid(row=0, column=0, padx=3)
 
-exit_button = Button(button_frame, text="EXIT", width=27, height=7)
+exit_button = Button(button_frame, text="EXIT", width=27, height=7, command=tk.quit)
 exit_button.grid(row=0, column=1)
 
-forward_button = Button(button_frame, text=">>", width=20, height=7)
+forward_button = Button(button_frame, text=">>", width=20, height=7, command=forward)
 forward_button.grid(row=0, column=2, padx=3)
 
 
 
-fact_label = Label(fact_frame, text=fact_list[20], font = ('Times', 15))
+fact_label = Label(fact_frame, text=fact_list[fact_turn], font = ('Times', 15))
 fact_label.place(x=100, y=150)
 
 tk.mainloop()
