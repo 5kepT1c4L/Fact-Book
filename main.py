6e4b194bdac8e36a00748dceaa3f73d34c2d8fe5
Eric Jing <70067413+5kepT1c4L@users.noreply.github.com>
2021-09-08 10:31:47 -0400

    Front end development finished

--- main.py
+++ main.py
@@ -23,5 +23,30 @@ print(fact_list)
 
 tk = Tk()
 tk.title('Fact Book')
-tk.geometry('500x500')
-tk.configure(bg="pink")
+tk.geometry('500x498')
+tk.configure(bg="white")
+
+
+#Frame to store the fact
+fact_frame = Frame(tk, highlightbackground="blue", highlightthickness=1,width=500, height=380, bd= 0, bg='light green')
+fact_frame.pack()
+fact_frame.pack_propagate(False)
+
+
+#Frame to store the buttons
+button_frame = Frame(tk, highlightbackground="blue", highlightthickness=1,width=500, height=120, bd= 0, bg='pink')
+button_frame.pack()
+button_frame.pack_propagate(False)
+
+
+#Buttons
+back_button = Button(button_frame, text="<<", width=20, height=7)
+back_button.grid(row=0, column=0, padx=3)
+
+exit_button = Button(button_frame, text="EXIT", width=27, height=7)
+exit_button.grid(row=0, column=1)
+
+forward_button = Button(button_frame, text=">>", width=20, height=7)
+forward_button.grid(row=0, column=2, padx=3)
+
+tk.mainloop()
