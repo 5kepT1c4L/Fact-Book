b1745d4ab857a501dfcb1f559f4885a746b5aee3
Eric Jing <70067413+5kepT1c4L@users.noreply.github.com>
2021-09-08 08:44:28 -0400

    Book's list set up

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+from tkinter import *
+
+fact_list = []
 
 info_website = requests.get('https://bestlifeonline.com/useless-facts/').text
 
@@ -9,7 +12,16 @@ facts = soup.find_all('h2', class_ = "header-mod")
 
 for fact in facts:
 
-    the_fact = fact.find('div', class_ = "title").text
+    the_fact = fact.find('div', class_ = "title").text.replace(".", "")
 
-    print(the_fact)
+    fact_list.append(the_fact)
 
+
+print(fact_list)d
+
+
+
+tk = Tk()
+tk.title('Fact Book')
+tk.geometry('500x500')
+tk.configure(bg="pink")
